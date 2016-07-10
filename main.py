5d21937785ce04b87ed299ded25ae819d917970d
Leilani Ann Raranga <retrosole@gmail.com>
2016-07-10 15:32:18 -0700

    Added .encode()
    feat: Added built-in encode() function for rot13 and is now working as
    intended

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
-
 	def render_str(self, template, **params):
 		# **params = extra paramaters
 		t = jinja_env.get_template(template)
 		# load and render template
 		return t.render(params)
-
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
 class MainHandler(Handler):
-    def get(self):
-    	# .get_all function that returns a list of all values that 
-    	# belong to string that matches a key in our query parameter
-    	self.render("index.html")
+	def get(self):
+		self.render("text_area.html")
+	def post(self):
+		rot13=""
+		user_input = self.request.get('text')
+		if user_input:
+			rot13=user_input.encode("rot13")
+		self.render("text_area.html", user_input=rot13)
+
+
+
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 ], debug=True)
